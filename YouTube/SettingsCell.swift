2ff9f91e2baf0e5810149f21c9988d4227c33e9b
LeeKyungjin <reyaong@users.noreply.github.com>
2017-05-25 12:08:00 +0900

    Setting menu model and configure slidein menu

---
+++ YouTube/SettingsCell.swift
@@ -0,0 +1,59 @@
+//
+//  SettingsCell.swift
+//  YouTube
+//
+//  Created by reyaong on 5/25/17.
+//  Copyright © 2017 reyaong. All rights reserved.
+//
+
+import UIKit
+
+class SettingsCell: BaseCell {
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? .darkGray : .white
+            nameLabel.textColor = isHighlighted ? .white : .black
+            iconImageView.tintColor = isHighlighted ? .white: .darkGray
+        }
+    }
+    
+    var setting: Setting? {
+        didSet {
+            nameLabel.text = setting?.name
+            
+            if let imageName = setting?.imageName {
+                iconImageView.image = UIImage(named: imageName)?.withRenderingMode(.alwaysTemplate)
+                iconImageView.tintColor = .darkGray
+      
+            }
+        }
+    }
+    
+    let nameLabel: UILabel = {
+    
+        let label = UILabel()
+        label.text = "Settings"
+        label.font = UIFont.systemFont(ofSize: 13)
+        return label
+    }()
+    
+    let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "settings")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        addSubview(nameLabel)
+        addSubview(iconImageView)
+        
+        addConstrainswithFormat(format: "H:|-8-[v0(30)]-8-[v1]|", views: iconImageView, nameLabel)
+        addConstrainswithFormat(format: "V:|[v0]|", views: nameLabel)
+        addConstrainswithFormat(format: "V:[v0(30)]", views: iconImageView)
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+    }
+}
--- YouTube/SettingsLauncher.swift
+++ YouTube/SettingsLauncher.swift
@@ -8,7 +8,17 @@
 
 import UIKit
 
-class SettingsLauncher: NSObject {
+class Setting: NSObject {
+    let name: String
+    let imageName: String
+    
+    init(name: String, imageName: String) {
+        self.name = name
+        self.imageName = imageName
+    }
+}
+
+class SettingsLauncher: NSObject, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     let blackView = UIView()
     
@@ -17,6 +27,13 @@ class SettingsLauncher: NSObject {
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
         cv.backgroundColor = .white
         return cv
+    }()
+    
+    let cellId = "cellId"
+    let cellHeight: CGFloat = 50
+    
+    let settings: [Setting] = {
+        return [Setting(name: "Settings", imageName: "settings"), Setting(name: "Terms & privacy policy", imageName: "privacy"), Setting(name: "Send Feedback", imageName: "feedback"), Setting(name: "Help", imageName: "help"), Setting(name: "Switch Account", imageName: "switch_account"), Setting(name: "Cancel", imageName: "cancel")]
     }()
     
     func showSettings() {
@@ -30,7 +47,7 @@ class SettingsLauncher: NSObject {
             window.addSubview(blackView)
             window.addSubview(collectionView)
             
-            let height: CGFloat = 200
+            let height: CGFloat = CGFloat(settings.count) * cellHeight
             let y = window.frame.height - height
             collectionView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
             
@@ -54,8 +71,43 @@ class SettingsLauncher: NSObject {
             }
         }
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! SettingsCell
+        let setting = settings[indexPath.item]
+        cell.setting = setting
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 
     override init() {
         super.init()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.register(SettingsCell.self, forCellWithReuseIdentifier: cellId)
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
